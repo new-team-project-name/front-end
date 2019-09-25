6c15c59982537dcef9c1a8d1dc7c415895f084d7
ehaffey <e-haffey@hotmail.com>
2019-09-25 16:27:32 -0400

    Add event creator
    --Setup initial js files to send new event to API.

---
+++ assets/scripts/event/ui.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const updateFeed = function () {
+// code
+}
+
+const eventCreateFailure = function () {
+// more code
+}
+
+module.exports = {
+  updateFeed,
+  eventCreateFailure
+}
