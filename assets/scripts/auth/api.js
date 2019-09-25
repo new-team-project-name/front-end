d5488fd78f270e3dce13805cad1b7e6956c88971
ehaffey <e-haffey@hotmail.com>
2019-09-25 13:07:44 -0400

    Begin auth setup
    Added initial api.js file in auth folder and began work on curl
    scripts.

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const config = require('./../config')
+const store = require('./../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
