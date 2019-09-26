7da4070d37a4c3601d23ec49453945bd55817005
SorenHopkins <Soren.hopkins@gmail.com>
2019-09-26 10:21:33 -0400

    Create AJAX & API calls for hangout resource
    Created Index, Delete, & Update API calls and page events
    Began set-up to hook up with handlebars and update modal
    @SorenHopkins & @enbacon

---
+++ assets/scripts/hangout/api.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const config = require('./../config')
+const store = require('./../store')
+
+const onCreateHangout = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/hangout',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      hangout: {
+        title: data.title,
+        picture: data.picture,
+        description: data.description,
+        date: data.date,
+        location: data.location,
+        owner: store.user._id,
+        website: data.website
+
+      }
+    }
+  })
+}
+
+const getHangouts = function () {
+  return $.ajax({
+    url: config.apiUrl + '/hangouts'
+    // headers: {
+    //   Authorization: 'Token token=' + store.user.token
+    // }
+  })
+}
+
+const updateHangout = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/hangouts' + data.hangouts.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deleteHangout = function (id) {
+  return $.ajax({
+    url: config.apiUrl + '/hangouts' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  onCreateHangout,
+  getHangouts,
+  updateHangout,
+  deleteHangout
+}
